c2436f5748891160d242cdba961389c595c1b370
Gbemisola23 <>
2023-05-09 13:44:23 +0000

    re-installed django 3.2 and delted unwanted users in admin

--- boutiqueado/settings.py
+++ boutiqueado/settings.py
@@ -9,8 +9,10 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 import os
 from pathlib import Path
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,8 +27,8 @@ SECRET_KEY = 'django-insecure-qg41_6who5!%b12%58s7j=mm$=5_3#!w!=9yif&#f)fnr6mpa$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-gbemisola23-boutiqueado-g81cdq3u1w.us2.codeanyapp.com']
-
+ALLOWED_HOSTS = [
+    '8000-gbemisola23-boutiqueado-g81cdq3u1w.us2.codeanyapp.com', "localhost"]
 
 # Application definition
 
